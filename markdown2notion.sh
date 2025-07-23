6a9c5416d6960ec8d0725d7e4cf3faadb0096415
TianCai19 <525114566@qq.com>
2025-07-23 18:49:56 +0800

    Update Raycast script to bash version

---
+++ markdown2notion.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title markdown2notion
+# @raycast.mode fullOutput
+
+# Optional parameters:
+# @raycast.icon 🤖
+
+# Navigate to your project directory
+cd /Users/zz/codes/markdown-to-notion
+
+# Run the Node.js script
+node index.js
+
+# You can add a success message or handle errors here if needed
+# For example, to show a toast notification in Raycast:
+# echo "Markdown imported successfully!"
